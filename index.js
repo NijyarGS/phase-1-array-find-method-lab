dce35a9cbad280bf76a00e03bf88b6d5be3fad60
Nijyar Ghazi <nijyar360@gmail.com>
2022-09-15 18:53:02 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function superbowlWin(argument){
+let data = argument.find((el) => {
+    if (el.result == "W"){
+        return el.year
+    }
+
+})
+
+if (data){
+    return data.year
+}
+else {
+    return undefined
+}
+}
+
